55973920a4dc8e7d9db8506b56109f9f37f292ee
Metal Maniacs <dlarbotics@outlook.com>
2026-02-03 17:25:19 -0800

    Added comments in PIDController.java

--- src/main/java/frc/robot/subsystems/PIDController.java
+++ src/main/java/frc/robot/subsystems/PIDController.java
@@ -44,28 +44,55 @@ public PIDController(double kP, double kI, double kD, boolean resetOnOvershoot, 
     this.resetOnOvershoot=resetOnOvershoot;
     this.daveyDTrick=daveyDTrick;
 }
+    /**
+ * Sets new values for the pid variables
+ * 
+ * @param kP
+ *          the new p constnat
+ * @param kI
+ *          the new I constant
+ * @param kD
+ *          the new D constant
+ */
     public void setPID(double kP, double kI, double kD) {
         this.kP=kP;
         this.kI=kI;
         this.kD=kD;
     }
+    /**
+     * calculates the output using the PIDController
+     * 
+     * @param targetValue
+     *          The desired number 
+     * @param currentValue
+     *          the current value of whatever you're using (Gyro, Encoder)
+     * @return
+     *          the output using the p, i, and d values set in the constructor
+     */
     public double calculate(double targetValue, double currentValue)  {
     
     double error=currentValue-targetValue;
 
-    if (restOnOvershoot&&Math.signum(error) !=Math.signum(lastError)) {
+    //reset if I should on overshoot, and I overshot
+    if (resetOnOvershoot&&Math.signum(error)!=Math.signum(lastError)) {
         totalError=0;
     }
 
     totalError+=error;
-    double pChange=-kP*error;
+    double pChange=-kP*error; //If my error is negative, spin moters faster
     double iChange=-kI*totalError;
 
     if (!daveyDTrick) {
-        double dChange=-kd*(error-lastError);
-        double output=pChange+iChnage+dChange;
+        double dChange=-kD*(error-lastError); //(y2-y1)/(x2-x1)*constant
+        double output=pChange+iChange+dChange;
         lastError=error;
 
+        return output;
+    }
+    else {
+        double defaultOutput=pChange+iChange;
+        double output=defaultOutput+kD*Math.signum(defaultOutput);
+        lastError=error;
         return output;
     }
     public void setValues(PIDValues vales) {
